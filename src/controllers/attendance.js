5a389fd9d901a9496ce09e38c9029fe1520b7baa
Diego Lopes <diego-chez@hotmail.com>
2021-02-20 15:42:33 -0300

    create basic post route for attendance

--- src/controllers/attendance.js
+++ src/controllers/attendance.js
@@ -1,5 +1,9 @@
 module.exports = app => {
     app.get("/atendimentos", (req, res) => {
-        res.send("Get:: rota de atendimentos")
+        res.send("GET:: rota de atendimentos")
+    })
+
+    app.post("/atendimentos", (req, res) => {
+        res.send("POST:: rota de atendimentos")
     })
 }
