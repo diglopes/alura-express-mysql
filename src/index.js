cd3b452d66b66c383d21ec1bae0560b950f6dec5
Diego Lopes <diego-chez@hotmail.com>
2021-02-20 17:21:40 -0300

    create attendance table on server bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const customExpress = require("../config/customExpress")
 const conn = require("./infra/connetion")
+const tables = require("./infra/tables")
 
 const app = customExpress()
 const PORT = 3000
@@ -9,6 +10,7 @@ conn.connect((err) => {
         console.log(err);
     } else {
         console.log("Database connected");
+        tables.init(conn)
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`);
         })
---
+++ src/infra/tables.js
@@ -0,0 +1,19 @@
+class Tables {
+    init(conn) {
+        this.conn = conn
+        this.createAttendanceTable()
+    }
+
+    createAttendanceTable() {
+        const sql = "CREATE TABLE IF NOT EXISTS atendimentos (id int NOT NULL AUTO_INCREMENT primary key, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text);"
+        this.conn.query(sql, (err) => {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log("Attendance table created");
+            }
+        })
+    }
+}
+
+module.exports = new Tables()
