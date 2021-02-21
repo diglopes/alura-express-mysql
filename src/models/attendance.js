cb82e60d611db2e948afb897031e0ecf3df4c29a
Diego Lopes <diego-chez@hotmail.com>
2021-02-20 21:03:05 -0300

    add schedule date and created at columns

--- src/models/attendance.js
+++ src/models/attendance.js
@@ -1,9 +1,19 @@
+const moment = require("moment")
 const conn = require("../infra/connetion")
 
 class AttendanceModel {
     create(attendance) {
+        const dateMask = "YYYY-MM-DD HH:mm:ss"
+        const createdAt = moment().format(dateMask)
+        const normalizedDate = moment(attendance.data, "DD/MM/YYYY").format(dateMask)
         const sql = "INSERT INTO atendimentos SET ?"
-        conn.query(sql, attendance, (err, response) => {
+        const composedAttendance = {
+            ...attendance,
+            data_criacao: createdAt,
+            data: normalizedDate,
+        }
+        
+        conn.query(sql, composedAttendance, (err, response) => {
             if(err) {
                 console.log(err);
             } else {
